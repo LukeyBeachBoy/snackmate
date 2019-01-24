3d7d90064313fd045da1fb213c46968f2b70c419
LukeyBeachBoy <lb580@kent.ac.uk>
2019-01-24 18:38:10 +0000

    Added side bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  sideBarOpen = false;
+  username = 'Not logged in';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  toggleSidebar() {
+    if (!this.sideBarOpen) {
+      $('#sideBar').css({ width: '250px' });
+      $('#main').css({ 'margin-left': '250px' });
+      $('#sidebarBtn').css({ transform: 'rotate(90deg)' });
+      this.sideBarOpen = true;
+    } else {
+      $('#sideBar').css({ width: '0' });
+      $('#main').css({ 'margin-left': '0' });
+      $('#sidebarBtn').css({ transform: 'rotate(0)' });
+      this.sideBarOpen = false;
+    }
   }
-
 }
