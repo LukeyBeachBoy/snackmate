2069b0a1f53b105fd9bdfff089115275ea783dd3
Luke <lb580@kent.ac.uk>
2019-02-10 01:33:31 +0000

    :memo: The recipe builder component is now a parent to the two steps it will take for a user to create a recipe. This will allow it to oversee the data created in step one, and store it until the data from step 2 is ready, where they will be combined and joined with the nutrition data before final upload

--- src/app/recipe-builder/recipe-builder.component.ts
+++ src/app/recipe-builder/recipe-builder.component.ts
@@ -1,83 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Location } from '@angular/common';
-import * as $ from 'jquery';
-import { NgForm, NgModel } from '@angular/forms';
-import { RecipeService } from '../services/recipe.service';
-import { Recipe } from '../services/recipe.model';
-import { AuthService } from '../services/auth.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-builder',
   templateUrl: './recipe-builder.component.html',
   styleUrls: ['./recipe-builder.component.scss']
 })
-export class RecipeBuilderComponent implements OnInit {
-  selectedFile: File = null;
-  localUrl = '';
-  incompleteSubmit = false;
-  constructor(
-    private loc: Location,
-    private http: HttpClient,
-    private recipeSvc: RecipeService,
-    private auth: AuthService
-  ) {}
-  ngOnInit() {
-    $('.custom-file-label').css('font-size', '1.15em');
-  }
-  onImageSelect(event) {
-    this.selectedFile = <File>event.target.files[0];
-    if (this.selectedFile) {
-      const reader = new FileReader();
-      reader.onload = e => (this.localUrl = reader.result as string);
-      reader.readAsDataURL(this.selectedFile);
-
-      let fieldVal: string = $('#recipePic').val() as string;
-      fieldVal = fieldVal.replace('C:\\fakepath\\', '');
-      if (fieldVal !== undefined || fieldVal !== '') {
-        $('.custom-file-label').attr('data-content', fieldVal);
-        $('.custom-file-label').text(fieldVal);
-      }
-    }
-  }
-  checkStatus(form: NgForm) {
-    if (form.status === 'VALID') {
-      this.incompleteSubmit = false;
-    }
-  }
-  onUpload(form: NgForm) {
-    if (form.invalid) {
-      return (this.incompleteSubmit = true);
-    }
-    const recipe: Recipe = {
-      allergens: [],
-      date: null,
-      description: form.value.description,
-      imageURL: '',
-      ingredients: [],
-      instructions: [],
-      name: form.value.name,
-      nutrition: {
-        calories: 0,
-        carbs: 0,
-        fat: 0,
-        protein: 0
-      }
-    };
-    this.auth.user$.subscribe(
-      user => {
-        if (user) {
-          recipe.userId = user.uid;
-          this.recipeSvc.createRecipe(recipe, this.selectedFile);
-          this.loc.go('');
-        }
-      },
-      err => {}
-    );
-  }
-  onDeletePic(imageCtrl: NgModel) {
-    imageCtrl.reset(); // Reset the form to empty so the user can choose another pic
-    this.localUrl = ''; // Tell other elements that the url is empty so they can update
-    $('#fileName').text('Choose File'); // Add the place holder back in
-  }
-}
+export class RecipeBuilderComponent {}
