bb6daebafb811973bd89dfa0b304bc32a8feeda5
LukeyBeachBoy <lb580@kent.ac.uk>
2019-02-10 16:33:54 +0000

    :zap: Overhauled side bar to force GPU acceleration so that the animation works on chrome on Ios

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -33,17 +33,10 @@ export class SidebarComponent implements OnInit {
      * and resize the sidebar if the client is on mobile/desktop
      */
     if ($('.mobile').css('float') === 'none') {
-      if (this.sidebarOpen) {
-        $('.sideMenu')
-          .addClass('mobile')
-          .removeClass('desktop');
-      }
+      // Resize for mobile
+      $('#sideBar').css({ width: '196px' });
     } else {
-      if (this.sidebarOpen) {
-        $('.sideMenu')
-          .addClass('desktop')
-          .removeClass('mobile');
-      }
+      $('#sideBar').css({ width: '250px' });
     }
   }
 
@@ -51,34 +44,55 @@ export class SidebarComponent implements OnInit {
     /* This is the function called each time the sidebar button is
      pressed */
     this.sidebar.getStatus().subscribe(() => {
-      if (!this.sidebarOpen) {
-        if ($('.mobile').css('float') === 'none') {
+      this.buttonToggle();
+      // If mobile
+      if ($('.mobile').css('float') === 'none') {
+        if (!this.sidebarOpen) {
           $('#sideBar')
             .addClass('openMobile')
-            .removeClass('close');
-          // $('#sideBar').css({ width: '196px' });
+            .removeClass('closedMobile')
+            .removeClass('open')
+            .removeClass('closed');
+          this.sidebarOpen = true;
         } else {
-          // $('#sideBar').css({ width: '250px' });
           $('#sideBar')
-            .addClass('open')
-            .removeClass('close');
+            .addClass('closedMobile')
+            .removeClass('openedMobile')
+            .removeClass('open')
+            .removeClass('closed');
+          this.sidebarOpen = false;
         }
-
-        $('#sidebarBtn').css({
-          transform: 'rotate(90deg)',
-          transition: '0.4s'
-        });
-        this.sidebarOpen = true;
       } else {
-        // $('#sideBar').css({ width: '0' });
-        $('#sideBar')
-          .addClass('close')
-          .removeClass('open openMobile');
-        $('#sidebarBtn').css({ transform: 'rotate(0)' });
-        this.sidebarOpen = false;
+        if (!this.sidebarOpen) {
+          $('#sideBar')
+            .addClass('opened')
+            .removeClass('closed')
+            .removeClass('closedMobile')
+            .removeClass('openMobile');
+          this.sidebarOpen = true;
+        } else {
+          $('#sideBar')
+            .addClass('closed')
+            .removeClass('opened')
+            .removeClass('closedMobile')
+            .removeClass('openMobile');
+          this.sidebarOpen = false;
+        }
       }
     });
   }
+
+  buttonToggle() {
+    if (!this.sidebarOpen) {
+      $('#sidebarBtn').css({
+        transform: 'rotate(90deg)',
+        transition: '0.4s'
+      });
+    } else {
+      $('#sidebarBtn').css({ transform: 'rotate(0)' });
+    }
+  }
+
   onNav() {
     this.sidebar.toggleSideBar();
   }
