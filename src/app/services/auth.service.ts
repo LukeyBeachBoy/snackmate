7f79f2b9a7e7cbbada3fd0d843cc8dc5cea01538
Juned-h <45270791+Juned-h@users.noreply.github.com>
2019-03-26 16:06:47 +0000

    Followers Array Fixed
    Followers and Following field arrays are now created if it doesn't exist or kept if already exists without clearing out the array.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -56,27 +56,46 @@ export class AuthService {
     return this.router.navigate(['/']);
   }
 
-  private updateUserData({ uid, email, displayName, photoURL}: User) {
+  private updateUserData({ uid, email, displayName, photoURL }: User) {
     const userRef: AngularFirestoreDocument<User> = this.afs.doc(
       `users/${uid}`
     );
-	
-	
 
-    const data = {
-      uid,
-      email,
-      displayName,
-      photoURL,
-	  following: [],
-	  followers: []
-    };
-	
-	
-	
-	
+    userRef.get().subscribe(user => {
+ 
+      const followers = user.get("followers");
+      if (followers) {
+        const data = {
+          uid,
+          email,
+          displayName,
+          photoURL,
+        
+        };
+        return userRef.set(data, { merge: true });
+      }
+      else {
+        const data = {
+          uid,
+          email,
+          displayName,
+          photoURL,
+          following: [],
+          followers: []
+        };
+        return userRef.set(data, { merge: true });
+      }
+      
+    });
 
-    return userRef.set(data, {merge: true});
+
+  
+
+
+
+
+
+   
   }
 
   public getUser(uid: string) {
