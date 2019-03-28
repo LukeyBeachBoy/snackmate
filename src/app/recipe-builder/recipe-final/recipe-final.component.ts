095404e8e75c21c910886ce37ce8fe4ea4f2bf11
Luke Beach <lb580@kent.ac.uk>
2019-03-28 17:30:04 +0000

    Finished recipe-builder instructions page and connected it to the database so now the user can create a complete recipe

--- src/app/recipe-builder/recipe-final/recipe-final.component.ts
+++ src/app/recipe-builder/recipe-final/recipe-final.component.ts
@@ -2,11 +2,16 @@
  * @file Logic for the final page of the recipe builder
  * @author Luke Beach // lb580@kent.ac.uk
  */
+declare var require: any;
 
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+const array_move = require('array-move');
 import { AuthService } from 'src/app/services/auth.service';
 import { User } from 'src/app/definitions/user.model';
+import { BuilderService } from 'src/app/services/builder.service';
+import { RecipeService } from 'src/app/services/recipe.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-final',
@@ -19,7 +24,12 @@ export class RecipeFinalComponent implements OnInit {
   input = '';
   incompleteSubmit = false;
   count = 0;
-  constructor(private auth: AuthService) {}
+  constructor(
+    private auth: AuthService,
+    private builder: BuilderService,
+    private recipeSvc: RecipeService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.auth.user$.subscribe(user => {
@@ -34,9 +44,31 @@ export class RecipeFinalComponent implements OnInit {
     } else {
     }
   }
-  onUpload(form: NgForm) {
-    // this.recipeSvc.uploadRecipe(recipe, this.selectedFile);
-    // this.loc.go('');
+  removeInstruction(instructionIndex) {
+    this.instructions.splice(instructionIndex, 1);
+  }
+  moveUp(instructionIndex) {
+    this.instructions = array_move(
+      this.instructions,
+      instructionIndex,
+      instructionIndex - 1
+    );
+  }
+  moveDown(instructionIndex) {
+    this.instructions = array_move(
+      this.instructions,
+      instructionIndex,
+      instructionIndex + 1
+    );
+  }
+  onUpload() {
+    const currentRecipe = this.builder.getRecipe();
+    currentRecipe.instructions = this.instructions;
+    this.builder.updateRecipe(currentRecipe);
+    this.recipeSvc.uploadRecipe(currentRecipe, this.builder.getImage().image);
+    setTimeout(() => {
+      this.router.navigate(['']);
+    }, 2500);
   }
   checkStatus(form: NgForm) {
     if (form.status === 'VALID') {
